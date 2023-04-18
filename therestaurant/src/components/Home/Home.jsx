fd3731b0a3b4d183fb6d8c430d804ad29cb6b11b
d0ndada <pintoavila09@gmail.com>
2023-04-18 15:53:13 +0200

    Co-authored-by: amoano // home display

---
+++ therestaurant/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <header className="wrapper">Amos fine and dine</header>
+      <main className="wrapper">Info om resturang kort </main>
+      <footer className="wrapper">
+        <p className="footer-text">&copy; 2023 Amo Livs</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
---
+++ therestaurant/src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">HomePage</Link>
+        </li>
+        <li>
+          <Link to="/">HomePage</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
