468b7447e441335e3d31979313296368966e1589
d0ndada <pintoavila09@gmail.com>
2023-04-26 19:03:17 +0200

    amoanos kod

--- therestaurant/src/components/Layout/Layout.jsx
+++ therestaurant/src/components/Layout/Layout.jsx
@@ -1,22 +1,66 @@
 import "./Layout.css";
 import Navbar from "../Navbar/Navbar";
-import Routes from "../Routes/Routes"; // Add this line
 import Admin from "../Admin/Admin";
 import useBlockchain from "../../useContext/useBlockchain";
 import BlockchainContext from "../../BlockchainContext";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import RoutesComponent from "../Routes/Routes";
+import backgroundImage from "../../Images/mat-turkiskt.jpg";
 
 export const Layout = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const savedUsername = localStorage.getItem("username");
+    const savedPassword = localStorage.getItem("password");
+
+    if (savedUsername === "Admin" && savedPassword === "Admin") {
+      setLoggedIn(true);
+      if (location.pathname !== "/Booking") {
+        navigate("/Booking");
+      }
+    }
+  }, [navigate, location]);
+
+  const handleLogin = (username, password) => {
+    if (username === "Admin" && password === "Admin") {
+      console.log("Logged in successfully!");
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+      setLoggedIn(true);
+      navigate("/Booking");
+    } else {
+      console.log("Invalid username or password!");
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    setLoggedIn(false);
+    navigate("/");
+  };
+
+  const layoutStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    minHeight: "100vh",
+  };
   const blockchain = useBlockchain();
   return (
     <BlockchainContext.Provider value={blockchain}>
-      <div>
+      <div style={layoutStyle} className="wrapper">
         <header>
           <h1>Amos fine and dine</h1>
           <Admin />
-          <Navbar />
+          <Navbar loggedIn={loggedIn} onLogout={handleLogout} />
         </header>
         <main className="wrapper">
-          <Routes />
+          <RoutesComponent loggedIn={loggedIn} onLogin={handleLogin} />
         </main>
         <footer className="wrapper">
           <p className="footer-text">&copy; 2023 Amo Livs</p>
--- therestaurant/src/components/Routes/Routes.jsx
+++ therestaurant/src/components/Routes/Routes.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/Home";
 import BookingPage from "../pages/BookingPage/BookingPage";
+import Contact from "../pages/Contact/Contact";
+import AdminPage from "../pages/AdminPage/AdminPage";
+import About from "../pages/About/About";
 
-const RoutesComponent = () => {
+const RoutesComponent = ({ loggedIn, onLogin, onLogout }) => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/Booking" element={<BookingPage />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/About" element={<About />} />
+      <Route
+        path="/Admin"
+        element={
+          <AdminPage
+            loggedIn={loggedIn}
+            onLogin={onLogin}
+            onLogout={onLogout}
+          />
+        }
+      />
     </Routes>
   );
 };
