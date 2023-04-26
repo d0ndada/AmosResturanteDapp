44dbaeacf9890954b9b4c7e79ec6e1ba83bf90da
d0ndada <pintoavila09@gmail.com>
2023-04-26 14:41:03 +0200

    React stirct mode

--- therestaurant/src/index.js
+++ therestaurant/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
